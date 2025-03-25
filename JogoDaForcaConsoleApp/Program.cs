df47d45be68ef4329bcb694cf0cc0d925e7c3b46
ehachmann <ehachmann@hotmail.com>
2025-03-25 19:30:47 -0300

    Add project files.

---
+++ JogoDaForcaConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace JogoDaForca.ConsoleApp
+{
+    internal class Program
+    {
+
+        // Versão 1: Estrutura básica e entrada do usuário              
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine("              Jogo da Forca!               ");
+                Console.WriteLine("-------------------------------------------");
+
+                Console.WriteLine("Digite uma letra: ");
+                char chute = Console.ReadLine()[0];
+
+                Console.WriteLine(chute);
+
+                Console.ReadLine();
+
+
+            }
+
+        }
+    }
+}
