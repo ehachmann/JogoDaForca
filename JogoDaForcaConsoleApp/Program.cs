f7a76f7f2886e882c9152de2b3aaa1d30c917874
ehachmann <ehachmann@hotmail.com>
2025-03-25 19:53:03 -0300

    Versão 2: Exibir palavra secreta com traços

--- JogoDaForcaConsoleApp/Program.cs
+++ JogoDaForcaConsoleApp/Program.cs
@@ -3,18 +3,31 @@
     internal class Program
     {
 
-        // Versão 1: Estrutura básica e entrada do usuário              
+        // Versão 2: Exibir palavra secreta com traços            
         static void Main(string[] args)
         {
             while(true)
             {
+                string palavraSecreta = "MELANCIA";
+
+                char[] letrasEncontradas = new char[palavraSecreta.Length];
+
+                for (int caractere = 0; caractere < letrasEncontradas.Length, caractere++)
+                {
+                    letrasEncontradas[caractere] = '_';
+                   
+                }
+
+                string dicaDaPalavra = string.Join(" ", letrasEncontradas);
+
                 Console.Clear();
                 Console.WriteLine("-------------------------------------------");
                 Console.WriteLine("              Jogo da Forca!               ");
                 Console.WriteLine("-------------------------------------------");
+                Console.WriteLine($"Palavra secreta: {dicaDaPalavra}");
 
                 Console.WriteLine("Digite uma letra: ");
-                char chute = Console.ReadLine()[0];
+                char chute = Console.ReadLine()[0]; // Obtem apenas o primeiro caracter de uma string
 
                 Console.WriteLine(chute);
 
